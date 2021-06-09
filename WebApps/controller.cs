168b52442b468dd974eec71036e6649f80879f74
taku <21817808@bucks.ac.uk>
2021-06-09 18:18:36 +0100

    * .editorconfig: * Startup.cs: * Program.cs: * controller.cs: * WebApps.csproj: * appsettings.json: * Index.cshtml: * Error.cshtml: * favicon.ico: * Privacy.cshtml: * Error.cshtml.cs: * _ViewStart.cshtml: * Privacy.cshtml.cs: * _ViewImports.cshtml: * appsettings.Development.json: * launchSettings.json: * sps2.jpg: * sps1.jpg: * sps3.jpg: * Paper.jpg: * Stone.jpg: * jquery.js: * RPS_GUI.jpg: * Scissors.jpg: * EmptyClass.cs: * RPS_GUI_DP.jpg: * jquery.min.map: * EventHandlers.jpg: * ImageResources.jpg: * BitmapResources.jpg: * bootstrap.js: * bootstrap.css: * _ValidationScriptsPartial.cshtml: * bootstrap.min.js: * bootstrap.js.map: * bootstrap.css.map: * bootstrap-grid.css: * bootstrap.min.js.map: * bootstrap-reboot.css: * bootstrap.min.css.map: * bootstrap-grid.css.map: * jquery.validate.js: * LICENSE.txt: * bootstrap-grid.min.css: * bootstrap.bundle.js.map: * bootstrap.bundle.min.js: * bootstrap-reboot.css.map: * bootstrap-reboot.min.css: * additional-methods.js: * bootstrap.bundle.min.js.map: * bootstrap-grid.min.css.map: * bootstrap-reboot.min.css.map: * additional-methods.min.js: * jquery.validate.unobtrusive.min.js:
    * Index.cshtml.cs: added interface for app 03
    
    * APP 05.cs: ROCK PAPER SCISSORS, WHILST DISPLAYING IMAGES OF THUMBS

---
+++ WebApps/controller.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApps.Models;
+using ConsoleAppProject.App02;
+using ConsoleAppProject.App01;
+
+namespace WebApps.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DistanceConverter(DistanceConverter converter)
+        {
+            if (converter.FromDistance > 0)
+            {
+                converter.CalculateDistance();
+            }
+            return View(converter);
+        }
+
+        [HttpGet]
+        public IActionResult BMICalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BMICalculator(BMI_Calculator bmi)
+        {
+            if (bmi.Centimeters > 140)
+            {
+                bmi.CalculateMetric();
+            }
+            else if (bmi.Feet > 4 && bmi.Stones > 6)
+            {
+                bmi.CalculateImperial();
+            }
+            else
+            {
+                ViewBag.Error = "You have entered values too small for any adult!";
+                return View();
+            }
+
+            double bmiIndex = bmi.BmiIndex;
+
+            return RedirectToAction("HealthMessage", new { bmiIndex });
+        }
+        public IActionResult HealthMessage(double bmiIndex)
+        {
+            return View(bmiIndex);
+        }
+
+        public IActionResult StudentMarks()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
